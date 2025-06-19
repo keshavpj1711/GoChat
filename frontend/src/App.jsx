390ffdcf7a3ac6c48c0a1d996f454309fdd92950
keshavpj1711 <keshavpj1711@gmail.com>
2025-06-19 23:53:00 +0530

    fix: design fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import goLang from './assets/goLang.svg'
 
 function App() {
   return (
-    <>
-      <div className='p-2'>
-        <div className='flex gap-2 items-center'>
-          <img src={goLang} alt="goLang logo" className='w-8' />
+    <div className='h-lvh bg-sky-800'>
+      <div className='p-2 flex justify-center'>
+        <div className='flex gap-1 items-center rounded-xl bg-amber-50 w-32 p-2 justify-center shadow-xl'>
+          <img src={goLang} alt="goLang logo" className='w-7 -ml-1' />
           <p className='text-xl'>GoChat</p>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
