901b9194b2767285b072c196f81275061838b3f1
keshavpj1711 <keshavpj1711@gmail.com>
2025-06-20 21:50:18 +0530

    update: added the connection to handle chat history

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import Connection from './components/Connection'
+import { useState } from 'react'
+import ChatContainer from './components/ChatContainer'
 import Navbar from './components/Navbar'
+import { connect } from './api'
 
 function App() {
+  const [chatHistory, setChatHistory] = useState([])
+
+  // Calling the connect function in our components life-cycle
+  async function componentDidMount() {
+    connect((msg) => {
+      console.log("New Message: ", msg)
+      setChatHistory(
+        prevChatHistory => [
+          ...prevChatHistory, msg.data
+        ]
+      )
+      console.log(chatHistory)
+    })
+  }
+
   return (
-    <div className='h-lvh bg-sky-700'>
+    <div className='h-lvh bg-prussianBlue-50 flex flex-col'>
       <Navbar />
-      <Connection />
+      <ChatContainer messages={chatHistory} onEstbConn={componentDidMount}/>
     </div>
   )
 }
