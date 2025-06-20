96448b873291572441d0d48501b2e5f4e0ccd381
keshavpj1711 <keshavpj1711@gmail.com>
2025-06-20 14:50:56 +0530

    addn: to send msg to websocket and see the output in our frontend as well as backend

---
+++ frontend/src/components/Connection.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { connect, sendMsg } from "../api";
+
+const Connection = () => {
+  const [input, setInput] = useState("")
+
+  function handleClick() {
+    // console.log(input)
+
+    connect()
+
+    // Now here we can open the websocket connection
+    sendMsg(input)
+  }
+
+  function handleChange(e) {
+    // getting the input in the input tag and setting the state as it's value
+    setInput(e.target.value)
+  }
+
+  return (
+    <div className="my-2">
+      <div className="px-2 flex gap-2 justify-center">
+        <input
+          type="text"
+          placeholder="Enter text"
+          name="msg"
+          onChange={handleChange}
+          className="bg-amber-50 rounded-md p-1" />
+
+        <button onClick={handleClick}
+          className="bg-sky-500 p-2 rounded-md text-amber-50 hover:tras">
+          Send Msg
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Connection;
