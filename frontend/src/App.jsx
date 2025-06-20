a979224c9c1a1c44f51457cd86408ffe2a262645
keshavpj1711 <keshavpj1711@gmail.com>
2025-06-20 14:49:11 +0530

    update: updated the app.jsx to include components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import goLang from './assets/goLang.svg'
+import Connection from './components/Connection'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className='h-lvh bg-sky-800'>
-      <div className='p-2 flex justify-center'>
-        <div className='flex gap-1 items-center rounded-xl bg-amber-50 w-32 p-2 justify-center shadow-xl'>
-          <img src={goLang} alt="goLang logo" className='w-7 -ml-1' />
-          <p className='text-xl'>GoChat</p>
-        </div>
-      </div>
+    <div className='h-lvh bg-sky-700'>
+      <Navbar />
+      <Connection />
     </div>
   )
 }
