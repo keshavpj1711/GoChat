6b52256a07f76456e028ae58ecc7c2c900f14b81
keshavpj1711 <keshavpj1711@gmail.com>
2025-06-20 21:50:43 +0530

    update: updated new colors to use

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import goLang from "../assets/goLang.svg"
 const Navbar = () => {
   return (
     <div className='p-2 flex justify-center'>
-      <div className='flex gap-1 items-center rounded-xl bg-amber-50 w-32 p-2 justify-center shadow-xl'>
-        <img src={goLang} alt="goLang logo" className='w-7 -ml-1' />
+      <div className='flex gap-1 items-center rounded-xl bg-aliceBlue-50 w-full p-1 justify-center shadow-xl'>
+        <img src={goLang} alt="goLang logo" className='w-7 -ml-1 bg-aliceBlue-50 rounded-md py-1' />
         <p className='text-xl'>GoChat</p>
       </div>
     </div>
